243c8ed106c059404fbf917a9a8079b84e6066ff
santgeek <santiago.mordz@gmail.com>
2025-07-17 12:49:43 -0500

    build: user dashboard ans subsections

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
   /* config options here */
   
-=======
-  images: {
-    domains: ['www.freeiconspng.com'],
-  },
->>>>>>> ea01d7549bf9e8f3c4a1804b17b6fe4b08907154
 };
 
 export default nextConfig;
