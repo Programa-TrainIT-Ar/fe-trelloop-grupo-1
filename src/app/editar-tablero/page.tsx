9fb72e3adb1e0f0fac2b619eac350f251153033d
Sebastián <bichos1506@gmail.com>
2025-07-24 00:29:48 -0600

    modal en contsruccion

--- src/app/editar-tablero/page.tsx
+++ src/app/editar-tablero/page.tsx
@@ -1,14 +1,190 @@
 'use client';
-
+import { useState } from 'react';
+import { FaLock, FaGlobe, FaPlus, FaTag, FaSearch, FaTimes } from "react-icons/fa";
+import '@fortawesome/fontawesome-free'
 import React from 'react';
 
 const EditBoardPage: React.FC = () => {
+  const [tags, setTags] = useState<string[]>(["etiqueta"]);
+  const [newTag, setNewTag] = useState("");
+  const [visibility, setVisibility] = useState<"private" | "public">("private");
+  const handleAddTag = () => {
+    if (newTag.trim() !== "" && !tags.includes(newTag.trim())) {
+      setTags([...tags, newTag.trim()]);
+      setNewTag("");
+    }
+  };
+
+  const handleRemoveTag =(indexToRemove: number) =>{
+    setTags (prev => prev.filter ((_, i) => i !== indexToRemove));
+  }
+
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Editar Tablero</h1>
-      <p>Aquí estará el formulario de edición más adelante.</p>
-    </div>
+
+    <div className="min-h-screen bg-[#1c1c1c] text-white p-8">
+      <div className="max-w-3xl mx-auto space-y-8">
+        <div>
+          <label htmlFor="tablero" className="block font-medium mb-2 text-sm">
+            Nombre de tablero
+          </label>
+          <input
+            id="description"
+            type="text"
+            className="mt-2 p-3 pr-10 bg-[#313131B3] block w-full rounded-xl border-2 border-[#3C3C3CB2] backdrop-blur-[3.6px] text-base font-light text-white placeholder:text-[#797676] focus:outline-none focus:border-purple-500 bg-[#313131] h-[41px]"
+            placeholder="Escribe aquí"
+            required
+          />
+        </div>
+
+
+
+        <div>
+          <label htmlFor="descripcion" className="block font-medium mb-2 text-sm">
+            Descripción
+          </label>
+          <textarea
+            id="description"
+            className="mt-2 p-3 pr-10 bg-[#313131B3] block w-full rounded-xl border-2 border-[#3C3C3CB2] backdrop-blur-[3.6px] text-base font-light text-white placeholder:text-[#797676] focus:outline-none focus:border-purple-500 bg-[#313131] h-[127px]"
+            placeholder="Escribe aquí"
+            required
+          />
+
+        </div>
+        {/* Miembros */}
+        <div>
+          <label htmlFor="miembros" className="block font-medium mb-2 text-sm">
+            Miembros{' '}
+
+          </label>
+          <div className="relative">
+            <input
+              id="miembros"
+              type="text"
+              placeholder="Buscar por nombre o @usuario..."
+              className="mt-2 p-3 pr-10 bg-[#313131B3] block w-full rounded-xl border-2 border-[#3C3C3CB2] backdrop-blur-[3.6px] text-base font-light text-white placeholder:text-[#797676] focus:outline-none focus:border-purple-500 bg-[#313131] h-[41px]"
+            />
+            <button
+              type='button'
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            >
+              <FaSearch style={{ fontSize: "20px" }} />
+            </button>
+          </div>
+        </div>
+
+        {/* Etiquetas */}
+        <div>
+          <label htmlFor="etiquetas" className="block font-medium mb-2 text-sm">
+            Etiquetas{' '}
+
+          </label>
+          <div className="relative">
+            <input
+              id="etiquetas"
+              type="text"
+              className="mt-2 p-3 pr-10 bg-[#313131B3] block w-full rounded-xl border-2 border-[#3C3C3CB2] backdrop-blur-[3.6px] text-base font-light text-white placeholder:text-[#797676] focus:outline-none focus:border-purple-500 bg-[#313131] h-[41px]"
+              placeholder="Escribe un nombre de etiqueta para crearla..."
+              value={newTag}
+              onChange={(e) => setNewTag(e.target.value)}
+            />
+            <button
+              type="button"
+              onClick={handleAddTag}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
+            >
+              <FaPlus style={{ fontSize: "20px" }} />
+             
+            </button>
+          </div>
+          <div className="flex flex-wrap gap-2 mt-3">
+            {tags.map((tag, i) => (
+              <span
+                key={i}
+                className="px-3 py-1 rounded-full border border-gray-500 text-sm flex items-center gap-1"
+              >
+                <FaTag className="text-gray-400" />
+                {tag}
+                <FaTimes className="cursor-pointer' hover:text-red-400 transition"
+                onClick={()=>handleRemoveTag(i)}/>
+              </span>
+            ))}
+          </div>
+        </div>
+
+        {/* Visibilidad */}
+        <div>
+
+          <div className="space-y-3">
+            <label className="flex items-start gap-3 cursor-pointer">
+              <input
+                type="radio"
+                name="visibility"
+                value="private"
+                checked={visibility === 'private'}
+                onChange={() => setVisibility('private')}
+                className="mt-1 h-[18px] w-[18px] appearance-none rounded-full border border-gray-400 checked:bg-[var(--global-color-primary-600)]"
+                style={{ accentColor: 'var(--global-color-primary-600)' }}
+              />
+              <div className="flex items-start gap-2">
+                <FaLock className="text-white mt-1" />
+
+                <div className="flex flex-col leading-tight">
+                  <span className="text-sm font-medium text-white">Privado</span>
+                  <span className="text-xs text-gray-400">
+                    (Solo tú y los miembros invitados pueden verlo.)
+                  </span>
+                </div>
+              </div>
+            </label>
+
+            <label className="flex items-start gap-3 cursor-pointer">
+              <input
+                type="radio"
+                name="visibility"
+                value="public"
+                checked={visibility === 'public'}
+                onChange={() => setVisibility('public')}
+                className="mt-1 h-[18px] w-[18px] appearance-none rounded-full border border-gray-400 checked:bg-[var(--global-color-primary-600)]"
+                style={{ accentColor: 'var(--global-color-primary-600)' }}
+              />
+              <div className="flex items-start gap-2">
+
+                <FaGlobe className="text-white mt-1" />
+                <div className="flex flex-col leading-tight">
+                  <span>Público</span>
+
+                  <span className="text-xs text-gray-400">
+                    (Cualquier miembro del equipo puede acceder.)
+                  </span>
+                </div>
+              </div>
+            </label>
+          </div>
+        </div>
+
+
+        {/*Botones de cancelar y guardar*/}
+
+        <div className='pt-6 flex gap-2'>
+          <button
+            type='button'
+            className='text-state-default font-light border border-state-default rounded-lg px-16 py-2 text-sm hover:bg-background-medium transition'>
+            Cancelar edición
+          </button>
+          <button
+            type='button'
+            className='bg-state-default font-light text-white rounded-lg px-16 py-2 text-sm hover:bg-state-hover transition'>
+            Actualizar tablero
+          </button>
+        </div>
+      </div >
+    </div >
+
+
   );
 };
 
+
 export default EditBoardPage;
+
+
